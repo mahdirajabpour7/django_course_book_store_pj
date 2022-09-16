25915aa2ac73608ddbed4da996b9e1db3741ef95
mahdi <mahdirajabpour1380@gmail.com>
2022-09-17 01:57:04 +0430

    add detailview

--- books/urls.py
+++ books/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
 
-from .views import BookListViews
+from .views import BookListViews, BookDetailView
+
 
 urlpatterns = [
     path("", BookListViews.as_view(), name="book_list"),
+    path("<int:pk>/", BookDetailView.as_view(), name="book_detail"),
 
 ]
--- books/views.py
+++ books/views.py
@@ -8,6 +8,11 @@ from .models import Book
 
 class BookListViews(generic.ListView):
     model = Book
-    template = "books/book_list.html"
+    template_name = "books/book_list.html"
     context_object_name = "books"
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    template_name = "books/book_detail.html"
 
