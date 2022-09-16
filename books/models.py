8538b0d28d98db667f5b88c070f0033e0ed61940
mahdi <mahdirajabpour1380@gmail.com>
2022-09-16 14:08:33 +0430

    add Book models

--- books/models.py
+++ books/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    text = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    #1299.57
 
-# Create your models here.
+
+    def __str__(self):
+        return self.title
+
+
+
